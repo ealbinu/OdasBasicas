b3e0b5c02f5ffbaaf9518c4ec91bc207c1a1800c
Albin Rdz <ealbinu@gmail.com>
2020-10-02 19:48:52 -0500

    ES1T1-31
    Instrucciones invertidas

--- assets/modulos/selectable.js
+++ assets/modulos/selectable.js
@@ -49,7 +49,7 @@ Vue.component('selectable', {
        this.$emit('input', "")
     },
     template: `
-        <div class="selectable" :class="setclass"  @click="clicked">
+        <div class="selectable" :class="setclass + ' ' + (status ? 'on':'off')"  @click="clicked">
             <div :class="this.status ? 'circledactive' : ''"></div>
             <slot></slot>
             <div class="result" v-if="evaluate" :class="setclass + ' animate__animated animate__heartBeat'"></div>
