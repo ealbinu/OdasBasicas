de2227f14e3b815d8dd9c75f2d5ddf2eada41e24
Albin Rdz <ealbinu@gmail.com>
2020-10-13 17:47:49 -0500

    ma1t2-140 & assets

--- assets/modulos/utils.js
+++ assets/modulos/utils.js
@@ -5,3 +5,17 @@ Vue.component('checkmark', {
 Vue.component('markex', {
     template: `<img src="../../assets/aimg/markex.svg" class="animate__animated animate__heartBeat">`
 })
+
+
+
+Vue.component('imgbg', {
+    props: ['initclass', 'img'],
+    template: `
+        <div :class="'imgbgMod ' + initclass + ' animate__animated animate__pulse animate__delay-2s'">
+            <img :src="img" class="w-100 ">
+            <div class="inputs">
+                <slot></slot>
+            </div>
+        </div>
+        `
+})
